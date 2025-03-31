4ad3b963a11ca6ff652088b6c9de3ed1f91ba486
toyeafo <afolabiolatoye@gmail.com>
2025-03-31 18:51:19 -0400

    start of the project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/toyeafo/chirpy
+
+go 1.23.2
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "net/http"
+
+func main() {
+	serveMux := http.NewServeMux()
+	server := http.Server{Handler: serveMux, Addr: ":8080"}
+	server.ListenAndServe()
+}
